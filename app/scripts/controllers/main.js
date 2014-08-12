5e16f8229f5c89eaab0863799155afd8beb1072a
Froelund <kristian@froelund.net>
2014-08-12 03:35:26 +0000

    Initial commit, yo angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name akaudioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the akaudioApp
+ */
+angular.module('akaudioApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
