95e67617d0420032885378d2e3ffb166381051f7
Froelund <kristian@froelund.net>
2014-08-12 04:00:53 +0000

    Removed boilerplate

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -2,13 +2,13 @@
 
 /**
  * @ngdoc function
- * @name akaudioApp.controller:MainCtrl
+ * @name akaudioApp.controller:HomeCtrl
  * @description
- * # MainCtrl
+ * # HomeCtrl
  * Controller of the akaudioApp
  */
 angular.module('akaudioApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('HomeCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
